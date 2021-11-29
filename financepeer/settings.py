91840dcb7f672c05d6f0949a126bec20b6aa5004
Chiruhas-Lingamaneni <74900279+Chiruhas-Lingamaneni@users.noreply.github.com>
2021-11-29 22:57:25 +0530

    Update settings.py

--- financepeer/settings.py
+++ financepeer/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'assesment',
         'USER': 'postgres',
-        'PASSWORD':'chiru123',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT': '5432',
     }
